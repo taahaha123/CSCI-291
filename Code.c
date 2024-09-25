6e18c12abab4f592555563e1c5ca52cba938beb8
taahaha123 <mohammadtaaha2005@gmail.com>
2024-09-25 12:23:33 +0400

    Add files via upload

---
+++ Code.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#define Espresso_Beans 8
+#define Espresso_Water 30
+#define Espresso_Mocha 0
+
+#define Cappucino_Beans 8
+#define Cappucino_Water 30
+#define Cappucino_Mocha 70
